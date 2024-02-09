6f3e3924ea11f4e8cd47b73dff51b91ac7dffe88
Olivier Bossaer <olivier.bossaer@gmail.com>
2024-02-09 13:28:59 +0100

    Update APIs from Sodexo4You to Pluxee

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { readFile } from 'fs/promises';
 import type { SearchTransactionResponse, AddTransactionRequest, GetAccountInfoResponse, Transaction } from './firefly.model'
-import { authenticateUser, getAccountEvents, getWalletInfo, type ProductType } from './pluxee-services';
+import { authenticateUser, getWalletTransactionsIterator, getWalletInfo, type ProductCode } from './pluxee-services';
 
 import { CronJob } from 'cron';
 type Options = {
@@ -16,36 +16,38 @@ type Options = {
   sportCulture?: number
   transport?: number
   conso?: number
+  after?: string
 }
 
 type NumericKeys<T> = {
   [K in keyof T]: Required<T>[K] extends number ? K : never;
 }[keyof T];
-const accountMapping = new Map<ProductType, NumericKeys<Options>>([
-  ['BOOK', 'book'],
-  ['CONSO', 'conso'],
-  ['ECO', 'eco'],
-  ['GIFT', 'gift'],
-  ['LUNCH', 'lunch'],
-  ['SPORT_CULTURE', 'sportCulture'],
-  ['TRANSPORT', 'transport']
+const accountMapping = new Map<ProductCode, NumericKeys<Options>>([
+  ['ECP', 'conso'],
+  ['EEP', 'eco'],
+  ['EGP', 'gift'],
+  ['ELP', 'lunch'],
+  ['ECO', 'sportCulture']
 ]);
 
 async function loadOptions(path: string) {
   return JSON.parse(await readFile(path, 'utf8')) as Options;
 }
 
-async function check({ login, password, url: baseUrl, token: fireflyToken, ...options }: Options) {
+async function check({ login, password, url: baseUrl, token: fireflyToken, after, ...options }: Options) {
   console.info('Check Pluxee');
   const token = await authenticateUser(login, password);
-  const { balances } = await getWalletInfo(token);
-  for (const { productType, amountAvailable } of balances) {
-    const type = accountMapping.get(productType);
+  const { listOfProducts } = await getWalletInfo(token);
+  for (const { productCode, balance } of listOfProducts) {
+    const type = accountMapping.get(productCode);
     if (!type) continue;
     const accountId = options[type];
     if (!accountId) continue;
 
-    for await (const record of getAccountEvents(token, productType)) {
+    for await (const record of getWalletTransactionsIterator(token, productCode)) {
+      if (!record.amount || !('type' in record)) continue;
+      if (after && record.date <= after) continue;
+
       const externalId = `pluxee_${record.id}`;
       const searchTransactionResponse = await fetch(
         new URL(`/api/v1/search/transactions?query=external_id%3A%22${externalId}%22&page=1`, baseUrl),
@@ -58,22 +60,25 @@ async function check({ login, password, url: baseUrl, token: fireflyToken, ...op
 
       const transaction: Partial<Transaction> = {
         amount: Math.abs(record.amount).toFixed(2),
-        date: record.time,
+        date: record.date,
         external_id: externalId,
         tags: ['Pluxee']
       };
-      if ('transaction' in record) {
-        const { to, city } = record.transaction;
+      if (record.type === 'SPENDING') {
+        const { affiliateName, city } = record;
         transaction.type = 'withdrawal';
         transaction.source_id = accountId.toString();
-        transaction.destination_name = to;
-        transaction.description = `${to}, ${city}`;
-      } else if ('order' in record) {
-        const { unitPrice, units, from } = record.order;
+        transaction.destination_name = affiliateName;
+        transaction.description = `${affiliateName}, ${city}`;
+      } else if (record.type === 'DEPOSIT') {
+        const { faceValue, numberOfUnit, clientName } = record;
         transaction.type = 'deposit';
-        transaction.source_name = from;
-        transaction.description = `${from}: ${units} x ${unitPrice.toFixed(2)} € = ${(units * unitPrice).toFixed(2)}`;
+        transaction.source_name = clientName;
+        transaction.description = `${clientName}: ${numberOfUnit} x ${faceValue.toFixed(2)} € = ${(numberOfUnit * faceValue).toFixed(2)}`;
         transaction.destination_id = accountId.toString();
+      } else {
+        console.warn(`Unmanaged transaction type: ${record.type}`);
+        continue;
       }
 
       await fetch(
@@ -91,17 +96,17 @@ async function check({ login, password, url: baseUrl, token: fireflyToken, ...op
         });
     }
 
-    const { data: { attributes: { current_balance: currentBalance, virtual_balance: virtualBalance } } } = await fetch(
+    const { data: { attributes: { current_balance: currentBalanceText, virtual_balance: virtualBalance } } } = await fetch(
       new URL(`/api/v1/accounts/${options[type]}`, baseUrl),
       { headers: { Authorization: `Bearer ${fireflyToken}` } }).then(async r => await r.json() as GetAccountInfoResponse);
 
-    const balance = parseFloat(currentBalance);
-    if (balance !== amountAvailable) {
+    const currentBalance = parseFloat(currentBalanceText);
+    if (balance !== currentBalance) {
       await fetch(
         new URL(`/api/v1/accounts/${options[type]}`, baseUrl),
         {
           method: 'put',
-          body: JSON.stringify({ virtual_balance: amountAvailable - balance + parseFloat(virtualBalance) }),
+          body: JSON.stringify({ virtual_balance: balance - currentBalance + parseFloat(virtualBalance) }),
           headers: {
             Authorization: `Bearer ${fireflyToken}`,
             'Content-Type': 'application/json'
@@ -112,7 +117,7 @@ async function check({ login, password, url: baseUrl, token: fireflyToken, ...op
 }
 
 loadOptions(process.env.OPTION_FILE ?? '/data/options.json')
-  .then(options => {
+  .then(async options => {
     CronJob.from({
       cronTime: options.cron,
       onTick: async () => { await check(options); }
